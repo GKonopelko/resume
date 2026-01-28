486dd87110a8f1c78c76f69ced3e6414140e9e69
Grigori Konopelko <grigori.konopelko@gmail.com>
2026-01-28 15:07:51 +0300

    Feat: add basic layout

---
+++ src/data/navigation.ts
@@ -0,0 +1,6 @@
+export const navigationLinks = [
+  { href: '#about', label: 'Обо мне' },
+  { href: '#skills', label: 'Навыки' },
+  { href: '#education', label: 'Образование' },
+  { href: '#experience', label: 'Опыт работы' },
+];
